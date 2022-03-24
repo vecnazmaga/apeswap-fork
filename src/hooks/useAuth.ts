5d6dcd77a58218ee9141e89aa5522f239940a981
vecnazmaga <vecnazmaga@gmail.com>
2022-03-24 16:36:34 -0500

    feat: Link on warnings

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -35,7 +35,10 @@ const useAuth = () => {
         } else {
           window.localStorage.removeItem(localStorageKey)
           if (error instanceof NoEthereumProviderError || error instanceof NoBscProviderError) {
-            toastError('Wallet Browser Required! To connect you need a web3 enabled browser.')
+            toastError('Use a crypto wallet app’s browser to connect to ApeSwap.', {
+              text: 'Learn More',
+              url: 'https://apeswap.gitbook.io/apeswap-finance/product-and-features/wallets/how-to-use-apeswap-on-mobile-devices',
+            })
           } else if (
             error instanceof UserRejectedRequestErrorInjected ||
             error instanceof UserRejectedRequestErrorWalletConnect
