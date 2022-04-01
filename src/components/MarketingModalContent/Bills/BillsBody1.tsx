e886b00db57af0fe7f79cad0b3ff4fef55fcca2d
vecnazmaga <vecnazmaga@gmail.com>
2022-04-01 20:55:07 +0100

    chore: - complete design for bills modal

--- src/components/MarketingModalContent/Bills/BillsBody1.tsx
+++ src/components/MarketingModalContent/Bills/BillsBody1.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import { BillsM1, BillsM2, BillsM3 } from '@apeswapfinance/uikit'
 import useTheme from 'hooks/useTheme'
-import { StyledText, Content, RightText, Description, TextButton, Hiw, MainContentBody, MainBody } from './styles'
+import {
+  StyledText,
+  Content,
+  RightText,
+  Description,
+  TextButton,
+  Hiw,
+  MainContentBody,
+  MainBody,
+  InnerTextButton,
+  Surround,
+  RightHeader,
+  RightContent,
+} from './styles'
 
 const BillsBody1: React.FC = () => {
   const theme = useTheme()
@@ -23,20 +36,34 @@ const BillsBody1: React.FC = () => {
       </Description>
 
       <MainContentBody>
-        <Content>
-          <BillsM1 width={48} bgColor={theme.theme.colors.white4} color={theme.theme.colors.text} />
-          <RightText>Click here to go to the liquidity provider. Select bill tokens and add liquidity.</RightText>
-        </Content>
+        <Surround>
+          <Content>
+            <BillsM1 width={50} bgColor={theme.theme.colors.white4} color={theme.theme.colors.text} />
+            <RightContent>
+              <RightHeader>Get LP</RightHeader>
+              <RightText>
+                <InnerTextButton href="https://apeswap.finance/add">Click here</InnerTextButton> to go to the liquidity
+                provider. Select bill tokens and add liquidity.
+              </RightText>
+            </RightContent>
+          </Content>
 
-        <Content>
-          <BillsM2 width={48} bgColor={theme.theme.colors.white4} color={theme.theme.colors.text} />
-          <RightText>Select your preferenced bill, and click the buy button.</RightText>
-        </Content>
+          <Content>
+            <BillsM2 width={50} bgColor={theme.theme.colors.white4} color={theme.theme.colors.text} />
+            <RightContent>
+              <RightHeader>Select and Buy</RightHeader>
+              <RightText>Select your preferenced bill, and click the buy button.</RightText>
+            </RightContent>
+          </Content>
 
-        <Content>
-          <BillsM3 width={48} bgColor={theme.theme.colors.white4} color={theme.theme.colors.text} />
-          <RightText>Receive your NFT, claim vested tokens or trade the NFT.</RightText>
-        </Content>
+          <Content>
+            <BillsM3 width={50} bgColor={theme.theme.colors.white4} color={theme.theme.colors.text} />
+            <RightContent>
+              <RightHeader>Receive and Claim</RightHeader>
+              <RightText>Receive your NFT, claim vested tokens or trade the NFT.</RightText>
+            </RightContent>
+          </Content>
+        </Surround>
       </MainContentBody>
     </MainBody>
   )
--- src/components/MarketingModalContent/Bills/styles.ts
+++ src/components/MarketingModalContent/Bills/styles.ts
@@ -3,7 +3,6 @@ import { Text } from '@apeswapfinance/uikit'
 
 export const MainBody = styled.div`
   height: 100%;
-  /* background: red; */
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
@@ -14,6 +13,16 @@ export const Description = styled.div`
   flex-direction: column;
   align-items: center;
   margin: 0em 1.5em;
+
+  ${({ theme }) => theme.mediaQueries.sm} {
+    margin-left: 5em;
+    margin-right: 5em;
+  }
+
+  ${({ theme }) => theme.mediaQueries.md} {
+    margin-left: 12em;
+    margin-right: 12em;
+  } ;
 `
 export const StyledText = styled(Text)`
   font-size: 12px;
@@ -22,7 +31,7 @@ export const StyledText = styled(Text)`
   text-align: center;
   margin: 0.5em 0;
 `
-export const TextButton = styled.button`
+export const TextButton = styled.a`
   border: none;
   background: none;
   color: ${({ theme }) => theme.colors.yellow};
@@ -31,6 +40,7 @@ export const TextButton = styled.button`
   line-height: 14px;
   font-weight: 500;
   margin: 0.5em 0;
+  text-align: center;
 
   &:hover {
     cursor: pointer;
@@ -41,32 +51,81 @@ export const Hiw = styled(StyledText)`
   color: ${({ theme }) => theme.colors.text};
   text-transform: uppercase;
   margin-top: 1em;
+
+  ${({ theme }) => theme.mediaQueries.md} {
+    margin-bottom: 0;
+  } ;
 `
 export const MainContentBody = styled.div`
   display: flex;
-  justify-content: space-evenly;
+  padding-left: 2em;
+  padding-right: 2em;
+
+  ${({ theme }) => theme.mediaQueries.sm} {
+    padding-left: 10em;
+    padding-right: 10em;
+  }
+
+  ${({ theme }) => theme.mediaQueries.md} {
+    margin-top: 0;
+  }
+
+  ${({ theme }) => theme.mediaQueries.xl} {
+    padding-left: 13em;
+    padding-right: 13em;
+  } ;
+`
+export const Surround = styled.div`
+  @media screen and (max-width: 280px) {
+    background-position: 10%;
+  }
+
+  display: flex;
+  align-self: center;
   flex-direction: column;
-  align-items: center;
-  /* background: yellow; */
-  margin-top: 1em;
+  background-image: url(/images/banners/line-bg.svg);
+  background-repeat: no-repeat;
+  background-position: 7%;
+  width: 100%;
+
+  ${({ theme }) => theme.mediaQueries.sm} {
+    background-position: 5%;
+  }
 `
 export const Content = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-evenly;
-  margin: 1em 2.5em;
-  /* background: green; */
-  width: 80%;
+  justify-content: space-between;
+  margin: 0;
+  margin-top: 1em;
+
+  &:first-child {
+    margin-top: 0;
+  }
+`
+export const RightContent = styled.div`
+  margin: 0 0 0 1em;
+
+  ${({ theme }) => theme.mediaQueries.sm} {
+    margin: 0;
+    width: 80%;
+  } ;
+`
+export const RightHeader = styled(StyledText)`
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.text};
+  text-align: left;
+  margin: 0;
+
+  ${({ theme }) => theme.mediaQueries.md} {
+  } ;
 `
 export const RightText = styled(StyledText)`
-  margin: 0 0 0 1em;
+  margin: 0;
   text-align: left;
-  width: 70%;
 `
-
-export const ModalBody = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+export const InnerTextButton = styled(TextButton)`
+  margin: 0;
+  padding: 0;
+  text-align: left;
 `
-export const RightContent = styled.div``
