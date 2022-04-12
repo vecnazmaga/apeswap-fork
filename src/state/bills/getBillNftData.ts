5dc2f5796e57b60e92103666308507ebbbe46c2f
vecnazmaga <vecnazmaga@gmail.com>
2022-04-11 19:07:48 -0500

    chore: Update api to axios

--- src/state/bills/getBillNftData.ts
+++ src/state/bills/getBillNftData.ts
@@ -1,10 +1,17 @@
 // import { apiBaseUrl } from 'hooks/api'
+import axiosRetry from 'axios-retry'
+import axios from 'axios'
+
 const apiBaseUrl = 'https://apeswap-api-development.herokuapp.com'
 
 const getBillNftData = async (billNftId: string) => {
   try {
-    const response = await fetch(`${apiBaseUrl}/bills/bsc/${billNftId}`)
-    const billNftDataResp = await response.json()
+    axiosRetry(axios, {
+      retries: 5,
+      retryCondition: () => true,
+    })
+    const response = await axios.get(`${apiBaseUrl}/bills/bsc/${billNftId}`)
+    const billNftDataResp = await response.data
     if (billNftDataResp.statusCode === 500) {
       return null
     }
@@ -16,8 +23,12 @@ const getBillNftData = async (billNftId: string) => {
 
 export const getNewBillNftData = async (billNftId: string, transactionHash: string) => {
   try {
-    const response = await fetch(`${apiBaseUrl}/bills/bsc/${billNftId}/${transactionHash}`)
-    const billNftDataResp = await response.json()
+    axiosRetry(axios, {
+      retries: 5,
+      retryCondition: () => true,
+    })
+    const response = await axios.get(`${apiBaseUrl}/bills/bsc/${billNftId}/${transactionHash}`)
+    const billNftDataResp = await response.data
     if (billNftDataResp.statusCode === 500) {
       return null
     }
