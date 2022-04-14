8bc3e2163bec519d29924ad4ecd2a4d79ced775d
vecnazmaga <vecnazmaga@gmail.com>
2022-04-14 19:13:27 -0400

    fix: Env var for API url

--- src/state/stats/getHomepageService.ts
+++ src/state/stats/getHomepageService.ts
@@ -1,7 +1,7 @@
 // import { apiBaseUrl } from 'hooks/api'
 import { ServiceData } from 'state/types'
 
-const apiBaseUrl = ' https://apeswap-api-development.herokuapp.com'
+export const apiBaseUrl = process.env.REACT_APP_API_BASE_URL || 'https://api.apeswap.finance'
 
 const getHomepageServiceStats = async (): Promise<ServiceData[]> => {
   try {
